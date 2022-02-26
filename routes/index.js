e397f83038042a165cf11a8f626e2e6e4776591a
ngvuong <vminguyen@ucdavis.edu>
2022-02-26 04:03:31 -0800

    Add new post route, update styles

--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,28 @@ const messages = [
   {
     text: 'Hi there!',
     user: 'Amando',
-    added: new Date(),
+    added: new Date().toDateString(),
   },
   {
     text: 'Hello World!',
     user: 'Charles',
-    added: new Date(),
+    added: new Date().toDateString(),
   },
 ];
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Message Board', messages });
+});
+
+router.post('/new', function (req, res, next) {
+  const { message, name } = req.body;
+  messages.push({
+    text: message,
+    user: name,
+    added: new Date().toDateString(),
+  });
+  res.redirect('/');
 });
 
 module.exports = router;
